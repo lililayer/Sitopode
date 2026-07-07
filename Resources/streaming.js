f1aa80ff7e92699134d1de03208fdcb012c7e922
Lili <163665831+lililayer@users.noreply.github.com>
2026-07-08 00:40:34 +0200

    Refactor Load function and streamline episode loading
    Refactored Load function to improve clarity and maintainability.

--- Resources/streaming.js
+++ Resources/streaming.js
@@ -47,43 +47,4 @@ video_player.addEventListener('ended', function() {
     }    
 });
 
-Load();const episodes_list = document.getElementById('episode_list');
-const video_player = document.getElementById('video_player');
-
-function Load() {
-    console.log("LOADING...");
-    video_sources = []
-    fetch('./Resources/episodes.json')
-        .then(response => response.json())
-        .then(data => {
-            // INIT
-            content = "<table style=\"background-color:black;margin-left:20px;margin-right:20px;margin-top:50px;display:block;overflow:scroll;\"><tr>"
-            // GET ALL EPISODES
-            data["episodes"].forEach(episode => 
-            {
-                video_sources.push(episode.path);
-                content += "<td style=\"padding:5px\"><button type=\"button\" style=\"border-color:#111;width:150px;height:150px;background-color:black;\">"
-                content += "<h4 style=\"text-align:left;padding-bottom:0px;padding-top:5px\">" + episode.tag + "</h4>"
-                content += "<p>" + episode._name + "</p>"
-                content += "</button></td>"
-            });
-        })
-        .then(() => {
-            content += "</tr></table>"
-            // INSERT IN HTML
-            episodes_list.insertAdjacentHTML('beforeend', content);
-            console.log(video_sources);
-            buttons = Array.from(episodes_list.getElementsByTagName('button'));
-            for (let i = 0; i < buttons.length; i++) {
-                console.log(i);
-                buttons[i].onclick = function() {
-                    video_player.src=video_sources[i];
-                    console.log(i);
-                    video_player.load();
-                };
-            };
-        });
-    return 0;
-}
-
 Load();
